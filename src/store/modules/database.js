bf5884bb6d9af1205a94686df664fad2943f4699
apgdjr <56176420+apgdjr@users.noreply.github.com>
2020-11-23 23:09:19 +0100

    commit database changes

--- src/store/modules/database.js
+++ src/store/modules/database.js
@@ -2,7 +2,8 @@ import { make } from "vuex-pathify";
 
 const state = {
   planks: [],
-  userID: "991a602c-b853-4a86-83f8-4627721f1b6f"
+  userID: "991a602c-b853-4a86-83f8-4627721f1b6f",
+  timer: "00:00:00"
 };
 
 const mutations = make.mutations(state);
