547014f9b91f56690acfe8c94f266224e7c731d9
apgdjr <56176420+apgdjr@users.noreply.github.com>
2020-11-28 16:55:20 +0100

    minor change

--- src/components/plankCounter.vue
+++ src/components/plankCounter.vue
@@ -1,54 +1,45 @@
 <template>
   <b-container>
     <b-row class="pt-4">
-      
-
       <b-col class="d-flex justify-content-center">
         <h3>{{ queryCounterInHours }}</h3>
       </b-col>
-
-      
     </b-row>
 
-     
-      <b-row>
-        <b-col class="d-flex justify-content-center">
-          <div v-if="checkIsStopped">
-            <b-button
-              size="lg"
-              variant="outline-primary"
-              v-on:click="commandStartCounter"
-            >
-            Start
-          </b-button>
-          </div>
-          
-          <div v-if="!checkIsStopped">
-            <b-button
-              size="lg"
-              variant="outline-primary"
-              v-on:click="commandStopCounter"
-            >
-            Stop
-          </b-button>
-          </div>       
-        </b-col>
-
-        <b-col class="d-flex justify-content-center">
+    <b-row>
+      <b-col class="d-flex justify-content-center">
+        <div v-if="checkIsStopped">
           <b-button
             size="lg"
-            variant="outline-secondary"
-            v-on:click="commandResetCounter"
-            :disabled='!checkIsStopped'
+            variant="outline-primary"
+            v-on:click="commandStartCounter"
           >
-            Reset
+            Start
           </b-button>
-        </b-col>
-      </b-row>
-     
+        </div>
 
-    
+        <div v-if="!checkIsStopped">
+          <b-button
+            size="lg"
+            variant="outline-primary"
+            v-on:click="commandStopCounter"
+          >
+            Stop
+          </b-button>
+        </div>
+      </b-col>
 
+      <b-col class="d-flex justify-content-center">
+        <b-button
+          size="lg"
+          variant="outline-secondary"
+          v-on:click="commandResetCounter"
+          :disabled="!checkIsStopped"
+        >
+          Reset
+        </b-button>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -65,25 +56,20 @@ export default {
   name: "plankCounter",
 
   setup(props, context) {
-    
     const store = context.root.$store;
-   
-    const checkIsStopped = computed (
-      ()=>{
-         
-        if (queryCounterStatus.value === 'stopped'){
-          //console.log('checkIsStopped: ', queryCounterStatus, true) 
-          return true
-        }
-        else{
-          //console.log('checkIsStopped: ', queryCounterStatus, false) 
-          return false
-        }
+
+    const checkIsStopped = computed(() => {
+      if (queryCounterStatus.value === "stopped") {
+        //console.log('checkIsStopped: ', queryCounterStatus, true)
+        return true;
+      } else {
+        //console.log('checkIsStopped: ', queryCounterStatus, false)
+        return false;
       }
-    )  
+    });
 
     const queryCounterInHours = computed(() => {
-      var secs = store.get('counter.counter');
+      var secs = store.get("counter.counter");
       var hours = Math.floor(secs / (60 * 60));
       if (hours < 10) hours = `0${hours}`;
 
@@ -97,43 +83,41 @@ export default {
 
       const value = `${hours}:${minutes}:${seconds}`;
 
-      console.log('storing... , ', value)
+      console.log("storing... , ", value);
       store.set("counter/counterInHours", value);
 
       return value;
     });
 
-
     const queryCounterStatus = computed(() => {
-      return store.get('counter.state') 
+      return store.get("counter.state");
     });
 
     //changes the state of the timer. switching from start/stop.
     function commandStartCounter() {
-        store.set("counter/state", "inProgress");
-        commandTickCounter();
+      store.set("counter/state", "inProgress");
+      commandTickCounter();
     }
 
-     //changes the state of the timer. switching from start/stop.
+    //changes the state of the timer. switching from start/stop.
     function commandStopCounter() {
       store.set("counter/state", "stopped");
     }
 
     function commandResetCounter() {
-      store.set("counter/counter", 0 );
+      store.set("counter/counter", 0);
     }
 
     function commandTickCounter() {
-      if (store.get('counter.state')  === "inProgress") {
+      if (store.get("counter.state") === "inProgress") {
         setTimeout(() => {
           //before adding to timer make sure that it is still in progress
-          if (store.get('counter.state')  === "inProgress") {
-            
-            let value = store.get('counter.counter');
+          if (store.get("counter.state") === "inProgress") {
+            let value = store.get("counter.counter");
             value = value + 1;
-            console.log('Counter Tick',value ) 
-            store.set("counter/counter", value++ );
-            
+            console.log("Counter Tick", value);
+            store.set("counter/counter", value++);
+
             commandTickCounter();
           }
         }, 1000);
